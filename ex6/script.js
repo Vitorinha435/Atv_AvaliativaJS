72e5eadbf4e51d9b96afa4b86d1fe42bb3665384
pedrocustodio790 <pedrocustodio2006@hotmal.com>
2025-06-03 17:09:12 -0300

    add

--- ex6/script.js
+++ ex6/script.js
@@ -1,19 +1,37 @@
 function configurar(){
-    let  textoinput = document.getElementById('texto');
-    let mensagebtn = document.getElementById(' btnmen ')
+    let  textoinput = document.getElementById('textoinput');
+    let mensagebtn = document.getElementById('btnmen')
 
 if((mensagebtn instanceof HTMLButtonElement)&& (textoinput instanceof HTMLInputElement)){
-    mensagebtn.addEventListener('click,',() =>{
-        let texto = textoinput.value;
-        validar(texto);
+    mensagebtn.addEventListener('click',() =>{
+        validar(textoinput.value);
         textoinput.value = '';
-
         });
     }
 
 }
-    function validar(textostr){
+    
+function validar(textostr){
         try{
-            if 
-        }
-    }
+            if (textostr > 70){
+                throw new Error('a mensagem deve ter no maximo 70 caracteres ')
+            }
+            if (textostr = ' '){
+                throw new Error('mensagem vazia');
+                }
+        }catch(erro){
+        exibir(erro.message, 'red')
+        }        
+}
+    
+function exibir(texto, cor) {
+        console.log(texto)
+  let saida = document.getElementById('mensagem');
+  if (saida instanceof HTMLParagraphElement) {
+    saida.textContent = texto;
+    saida.style.color = cor;
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', configurar);
