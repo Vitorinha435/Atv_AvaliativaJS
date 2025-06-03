d849a06195813f77d7f33a015b30e394b6a15fc2
Vitorinha435 <vitoria.aamro.tds24@gmail.com>
2025-06-03 17:18:21 -0300

    Commitando

---
+++ ex8/script.js
@@ -0,0 +1,12 @@
+const nomeInput = document.getElementById('nome');
+const enviarBtn = document.getElementById('enviar');
+const duplasLista= document.getElementById('duplas');
+
+if (nomeInput instanceof HTMLInputElement && enviarBtn instanceof HTMLButtonElement) {
+    let nomes = [];
+
+    enviarBtn.addEventListener('click', () => {
+      const entrada = nomeInput.value?.trim();
+   
+    })
+  }
